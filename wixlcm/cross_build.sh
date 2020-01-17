bafe106bbb2e09b54b76d07f267fba95f5513642
1000001101000 <38921757+1000001101000@users.noreply.github.com>
2020-01-17 10:33:35 -0600

    Create cross_build.sh

---
+++ wixlcm/cross_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##requires build-essential, linux-source, etc
+
+tar xf "/usr/src/$(ls /usr/src/ | grep linux-source)"
+
+make CROSS_COMPILE="arm-linux-gnueabi-" KERNEL_SOURCE="$(ls | grep linux-source)" all
+
